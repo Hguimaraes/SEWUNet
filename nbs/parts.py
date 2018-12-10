60bc0596c7fbd34e50ad598c9857d229ad1b8b40
Hguimaraes <hguimaraes@poli.ufrj.br>
2018-12-10 00:18:24 -0200

    testing a first approach

---
+++ nbs/parts.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils import data
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+
+class VSConvBlock(nn.Module):
+    """"""
+    def __init__(self, in_ch, out_ch, activation,
+                 dilation=1, padding=0, kernel_size=15):
+        """"""
+        super(VSConvBlock, self).__init__()
+        self.padding = padding
+
+        self.padding_layer = nn.ReflectionPad1d(self.padding)
+        self.conv = nn.Conv1d(in_ch, out_ch,
+                              kernel_size=kernel_size,
+                              stride=1,
+                              dilation=dilation)
+        self.batch = nn.BatchNorm1d(out_ch)
+        self.activation = activation
+
+    def forward(self, x):
+        # Block 1
+        x = self.padding_layer(x)
+        x = self.conv(x)
+        x = self.batch(x)
+        x = self.activation(x)
+
+        return x
+
+
+class DownSamplingBlock(nn.Module):
+    """Encoder block of the Fully-convolutional Network"""
+    def __init__(self, in_ch, out_ch, activation,
+                 padding=0, dilation=1, kernel_size=15):
+        super(DownSamplingBlock, self).__init__()
+        self.block = VSConvBlock(in_ch, out_ch,
+                                 activation=activation,
+                                 kernel_size=kernel_size,
+                                 padding=padding,
+                                 dilation=dilation)
+
+    def forward(self, x):
+        x = self.block(x)
+        return x[:, :, ::2], x
+
+
+class UpSamplingBlock(nn.Module):
+    """Decoder block of the Fully-convolutional Network"""
+    def __init__(self, in_ch, out_ch, activation, padding = None, kernel_size=5):
+        super(UpSamplingBlock, self).__init__()
+        self.padding = padding or (kernel_size // 2)
+        
+        # Convolution block
+        self.conv = VSConvBlock(in_ch, out_ch,
+                                activation=activation,
+                                kernel_size=kernel_size,
+                                padding=self.padding)
+        
+        # Deconvolution block
+        self.deconv = nn.ConvTranspose1d(
+            in_channels=in_ch - out_ch,
+            out_channels=in_ch - out_ch,
+            kernel_size=2,
+            stride=2,
+            padding=0,
+            output_padding=0,
+            bias=True,
+            dilation=1
+        )
+
+        self.deconv_activation = activation
+        
+    def forward(self, x, x_enc):
+        x = self.deconv(x)
+        x = self.deconv_activation(x)
+        x = torch.cat([x, x_enc], dim=1)
+        return self.conv(x)
+
+
+class OutBlock(nn.Module):
+    """Convolutional block similar to VSConvBlock.
+    The network input is fed into this layer"""
+    def __init__(self, in_ch, out_ch, activation, padding=0):
+        super(OutBlock, self).__init__()
+        self.conv = VSConvBlock(in_ch, out_ch,
+                                activation=activation,
+                                kernel_size=1,
+                                padding=padding)
+
+    def forward(self, x, x_enc):
+        x = torch.cat([x, x_enc], dim=1)
+        return self.conv(x) - x_enc
---
+++ nbs/utils.py
@@ -0,0 +1,62 @@
+import os
+import copy
+import string
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils import data
+import torch.nn.functional as F
+from librosa import output
+
+class ConfigObject:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+def reserve_pop(x):
+    # Helper function to reverse a list
+    return x[::-1][:-1]
+
+def id_generator(size=8, chars=string.ascii_uppercase + string.digits):
+    # https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def writer(x, y, out, sr, path):
+    n = x.shape[0]
+
+    for i in range(n):
+        # Generate a random hash
+        name = id_generator()
+
+        # Create paths
+        mix_pf = os.path.join(path, name + '_mixture.wav')
+        clean_pf = os.path.join(path, name + '_clean.wav')
+        sep_pf = os.path.join(path, name + '_separated.wav')
+
+        # Use librosa to write files
+        output.write_wav(mix_pf, x[i, 0, :].cpu().numpy(), sr)
+        output.write_wav(clean_pf, y[i, 0, :].cpu().numpy(), sr)
+        output.write_wav(sep_pf, out[i, 0, :].cpu().numpy(), sr)
+
+class LibriSpeechGenerator(data.Dataset):
+    """Pytorch generator for LibriSpeech
+    dataset and UrbanSound 8k noise"""
+    def __init__(self, config, X, y = None, mode="noise"):
+        self.n_samples = config.n_samples
+        self.mode = mode
+        
+        # Working condition
+        if self.mode == "noise" and y is None:
+            raise ValueError("y should be None for Noise mode")
+        
+        # Datasets
+        self.X, self.y = X, y
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+        if self.mode == "noise":
+            return self.X[index, :, :self.n_samples], self.y[index, :, :self.n_samples]
+        else:
+            return self.X[index, :, :self.n_samples], self.X[index, :, :self.n_samples]
